6bad713e1abebb5bd14d610af8dd5bf9a860f782
B Boy Breaker <breakersniper@hotmail.com>
2018-02-03 23:59:52 +0200

    initial ngx installation

---
+++ docs/readme.md
@@ -0,0 +1,9 @@
+# iko-demo
+
+Welcome to the project documentation!
+
+Use `npm run docs` for easier navigation.
+
+## Available documentation
+
+[[index]]
---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Keyboard } from '@ionic-native/keyboard';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { environment } from '@env/environment';
+import { CoreModule } from '@app/core';
+import { SharedModule } from '@app/shared';
+import { HomeModule } from './home/home.module';
+import { AboutModule } from './about/about.module';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production}),
+    FormsModule,
+    HttpModule,
+    TranslateModule.forRoot(),
+    NgbModule.forRoot(),
+    CoreModule,
+    SharedModule,
+    HomeModule,
+    AboutModule,
+    AppRoutingModule
+  ],
+  declarations: [AppComponent],
+  providers: [
+    Keyboard,
+    StatusBar,
+    SplashScreen
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
